d22a0c40c5c5ed9eba3171e65998462df6b7a398
vump19n <vump19@naver.com>
2018-10-15 18:12:42 +0900

    divided javascript

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-window.onload = function() {
-    var app = new Vue({
-        el: '#app',
-        data: {
-            message: '안녕하세요 Vue!'
-        }
-    });
-}
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+    }
+})
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [{
+            text: 'JavaScript 배우기'
+        }, {
+            text: 'Vue 배우기'
+        }, {
+            text: '무언가 멋진 것을 만들기'
+        }]
+    }
+})
